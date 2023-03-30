f5f88ed44d689c3db28ae39fd967c52ee8882ff9
njh7554 <njh7554@naver.com>
2023-03-30 17:48:03 +0900

    20230330

---
+++ 02_Jsp/src/main/java/ex07_jspl/Webtoon.java
@@ -0,0 +1,54 @@
+package ex07_jspl;
+
+import java.time.LocalDate;
+
+public class Webtoon {
+	private int webtoonNo;
+	private String title;
+	private double star;
+	private LocalDate uploadDate;
+	
+	public Webtoon() {
+		
+	}
+
+	public int getWebtoonNo() {
+		return webtoonNo;
+	}
+
+	public void setWebtoonNo(int webtoonNo) {
+		this.webtoonNo = webtoonNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getStar() {
+		return star;
+	}
+
+	public void setStar(double star) {
+		this.star = star;
+	}
+
+	public LocalDate getUploadDate() {
+		return uploadDate;
+	}
+
+	public void setUploadDate(LocalDate uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+
+	public Webtoon(int webtoonNo, String title, double star, LocalDate uploadDate) {
+		super();
+		this.webtoonNo = webtoonNo;
+		this.title = title;
+		this.star = star;
+		this.uploadDate = uploadDate;
+	}
+}
