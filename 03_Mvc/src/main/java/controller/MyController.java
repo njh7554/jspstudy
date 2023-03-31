0e4dc1aa6ca8a12ec2d46b455bbf79279ae4cd9c
njh7554 <njh7554@naver.com>
2023-03-31 17:40:01 +0900

    20230331

--- 03_Mvc/src/main/java/controller/MyController.java
+++ 03_Mvc/src/main/java/controller/MyController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import common.ActionForward;
 import model.MyAgeService;
 import model.MyBmiService;
 import model.MyService;
@@ -28,6 +29,8 @@ public class MyController extends HttpServlet {
 		String contextPath = request.getContextPath();                  /*  /03_Mvc           */
 		String urlMapping = requestURI.substring(contextPath.length()); /*  /today.do         */
 		
+		// ActionForward 객체 선언하기
+		ActionForward actionForward = null;
 		// MyService 인터페이스 타입의 myService 객체 선언하기
 		MyService myService = null;
 		
@@ -45,11 +48,18 @@ public class MyController extends HttpServlet {
 		}
 		
 		// 모델(서비스) 실행하기
-		String path = myService.execute(request, response);
+		if(myService != null) {
+			actionForward = myService.execute(request, response);
+		}
 		
 		// 응답 View로 이동하기
-		request.getRequestDispatcher(path).forward(request, response);
-		
+		if(actionForward != null) {
+			if(actionForward.isRedirect()) {
+				response.sendRedirect(actionForward.getPath());
+			} else {
+				request.getRequestDispatcher(actionForward.getPath()).forward(request, response);
+			}
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
